85b6ea0e63ffb8b84376dd6e7ddfa81a07fca1f2
r-jumper <ramos.cesar.f@hotmail.com>
2023-11-10 18:31:03 -0300

    Refatoração view login.

---
+++ habito_saudavel_astral_mvc/Controllers/PerfilController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace habito_saudavel_astral_mvc.Controllers
+{
+    public class PerfilController : Controller
+    {
+        public IActionResult Perfil()
+        {
+            return View();
+        }
+
+        public IActionResult Alimentação()
+        {
+            return View();
+        }
+
+        public IActionResult Treino()
+        {
+            return View();
+        }
+
+        public IActionResult Monitor()
+        {
+            return View();
+        }
+    }
+}
