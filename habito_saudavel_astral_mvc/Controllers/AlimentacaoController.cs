36204f1809190faf46ccdd59c027071e23688143
Fernando Ramos <58959339+r-jumper@users.noreply.github.com>
2023-11-13 21:06:31 -0300

    Criado controler alimentação com as views, grupos, receitas e refeições.

---
+++ habito_saudavel_astral_mvc/Controllers/AlimentacaoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace habito_saudavel_astral_mvc.Controllers
+{
+    public class AlimentacaoController : Controller
+    {
+        public IActionResult Receitas()
+        {
+            return View();
+        }
+
+		public IActionResult Grupos()
+		{
+			return View();
+		}
+		public IActionResult Refeicoes()
+		{
+			return View();
+		}
+	}
+}
