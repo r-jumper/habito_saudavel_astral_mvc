69b8f0a174dcd04bf74a22202384c9c85de9f443
r-jumper <ramos.cesar.f@hotmail.com>
2023-11-16 21:14:17 -0300

    Complementação no contorler e a view receitas.

--- habito_saudavel_astral_mvc/Controllers/ReceitasController.cs
+++ habito_saudavel_astral_mvc/Controllers/ReceitasController.cs
@@ -4,9 +4,34 @@ namespace habito_saudavel_astral_mvc.Controllers
 {
     public class ReceitasController : Controller
     {
-        public IActionResult Jantar()
+		public IActionResult Cafe()
+		{
+			return View();
+		}
+
+		public IActionResult Almoco()
+		{
+			return View();
+		}
+
+		public IActionResult Jantar()
         {
             return View();
         }
-    }
+
+		public IActionResult Lanche()
+		{
+			return View();
+		}
+
+		public IActionResult Nutritivo()
+		{
+			return View();
+		}
+
+		public IActionResult Sobremesa()
+		{
+			return View();
+		}
+	}
 }
